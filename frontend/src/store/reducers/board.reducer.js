3e96fe30c872376c1686adceba697f184cd48772
Keren Siebner <kerens969@gmail.com>
2023-01-17 21:51:01 +0200

    cleared all components with 'cart'

--- frontend/src/store/reducers/board.reducer.js
+++ frontend/src/store/reducers/board.reducer.js
@@ -5,21 +5,16 @@ export const ADD_BOARD = 'ADD_BOARD'
 export const UPDATE_BOARD = 'UPDATE_BOARD'
 export const REMOVE_BOARD = 'REMOVE_BOARD'
 export const UNDO_REMOVE_BOARD = 'UNDO_REMOVE_BOARD'
-// Cart
-export const ADD_TO_CART = 'ADD_TO_CART'
-export const CLEAR_CART = 'CLEAR_CART'
-export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+
 
 const initialState = {
   boards: [],
-  cart: [],
   lastRemovedBoard: null,
 }
 
 export function boardReducer(state = initialState, action) {
   var newState = state
   var boards
-  var cart
   switch (action.type) {
     case SET_BOARDS:
       newState = { ...state, boards: action.boards }
@@ -43,16 +38,6 @@ export function boardReducer(state = initialState, action) {
         board._id === action.board._id ? action.board : board
       )
       newState = { ...state, boards: boards }
-      break
-    case ADD_TO_CART:
-      newState = { ...state, cart: [...state.cart, action.board] }
-      break
-    case REMOVE_FROM_CART:
-      cart = state.cart.filter((board) => board._id !== action.boardId)
-      newState = { ...state, cart }
-      break
-    case CLEAR_CART:
-      newState = { ...state, cart: [] }
       break
     case UNDO_REMOVE_BOARD:
       if (state.lastRemovedBoard) {
