0d422891c3596619e09fa3284a64758a00f4754e
Darkfall48 <sidney@sebban.be>
2023-01-18 19:14:42 +0200

    Frontend - Improved: Task Preview

--- frontend/src/cmps/board/group/task/task-preview.jsx
+++ frontend/src/cmps/board/group/task/task-preview.jsx
@@ -6,39 +6,123 @@ import { VscEdit } from 'react-icons/vsc'
 import { utilService } from '../../../../services/util.service'
 
 export function TaskPreview({ task }) {
+  console.log('Task:', task)
   //? Private Components
-  function BackgroundArticle() {
+  function SetBackground() {
+    const { style } = task
     return (
       <article
         className="task-preview-background"
         style={{
-          backgroundColor: task
-            ? utilService.getRandomColor()
-            : task.style.bgColor,
+          backgroundColor: style.bgColor,
         }}
       ></article>
     )
   }
 
-  function LabelArticle() {
-    return <article className="task-preview-label">Labels</article>
+  function SetLabels() {
+    // TODO: Get labels as props
+    const labels = [
+      {
+        id: 'l101',
+        title: 'Done',
+        color: 'lightblue',
+      },
+      {
+        id: 'l102',
+        title: 'Progress',
+        color: 'orangered',
+      },
+      {
+        id: 'l103',
+        title: 'Todo',
+        color: 'orange',
+      },
+    ]
+    const { labelIds } = task
+    // const labelIds = null
+    if (!labelIds) return <article className="task-preview-labels"></article>
+    return (
+      <article className="task-preview-labels">
+        {labelIds.map((labelId) => {
+          const label = labels.find((label) => label.id === labelId)
+          return (
+            <span
+              className="task-preview-label"
+              key={label.id}
+              style={{ backgroundColor: label.color }}
+              title={label.title ? label.title : 'None'}
+            ></span>
+          )
+        })}
+      </article>
+    )
   }
-  return (
-    <section className="task-preview-section">
-      <BackgroundArticle />
-      <LabelArticle />
+
+  function SetTitle() {
+    return (
       <article className="task-preview-title">
         <p>{task.title}</p>
       </article>
+    )
+  }
+
+  function SetEditBtn() {
+    return (
       <article className="task-preview-edit">
-        <VscEdit />
+        <button>
+          <VscEdit />
+        </button>
       </article>
+    )
+  }
+
+  function SetInfos() {
+    return (
       <article className="task-preview-info">
         <AiOutlineEye /> <BsCheck2Square /> 0/5
       </article>
-      <article className="task-preview-member">Sidney, Keren, Yael</article>
-      {/* <NavLink to={`/toy/${toy._id}`}>Details</NavLink> |
-      <NavLink to={`/toy/edit/${toy._id}`}>Edit</NavLink> */}
+    )
+  }
+
+  function SetMembers() {
+    // TODO: Get members as props
+    const members = [
+      {
+        _id: 'u101',
+        fullname: 'Tal Tarablus',
+        imgUrl: 'https://robohash.org/',
+      },
+    ]
+
+    const { memberIds } = task
+
+    return (
+      <article className="task-preview-member">
+        {memberIds.map((memberId) => {
+          const member = members.find((member) => member._id === memberId)
+          const { _id, imgUrl, fullname } = member
+          return (
+            <img
+              key={_id}
+              className="task-preview-img"
+              src={imgUrl + fullname}
+              alt={fullname}
+              title={fullname}
+            />
+          )
+        })}
+      </article>
+    )
+  }
+  return (
+    <section className="task-preview-section">
+      <SetBackground />
+      <SetLabels />
+      <SetTitle />
+      <SetEditBtn />
+      <SetInfos />
+      <SetMembers />
     </section>
   )
 }
