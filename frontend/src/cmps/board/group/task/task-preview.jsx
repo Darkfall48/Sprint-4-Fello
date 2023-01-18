c685130379249d2685f99666484f84ab46a09673
Darkfall48 <sidney@sebban.be>
2023-01-18 12:08:56 +0200

    Frontend - Implemented: Task Preview

--- frontend/src/cmps/board/group/task/task-preview.jsx
+++ frontend/src/cmps/board/group/task/task-preview.jsx
@@ -1,9 +1,15 @@
 export function TaskPreview({ task }) {
   return (
-    <article className="task-preview-article">
-      <h2>{task.title}</h2>
+    <section className="task-preview-section">
+      <article className="task-preview-background">Background color</article>
+      <article className="task-preview-label">Labels</article>
+      <article className="task-preview-title">
+        <p>{task.title}</p>
+      </article>
+      <article className="task-preview-info">Infos: 0/5 5/7</article>
+      <article className="task-preview-member">Sidney, Keren, Yael</article>
       {/* <NavLink to={`/toy/${toy._id}`}>Details</NavLink> |
       <NavLink to={`/toy/edit/${toy._id}`}>Edit</NavLink> */}
-    </article>
+    </section>
   )
 }
