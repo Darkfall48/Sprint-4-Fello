8f48ce6d992c3d4754eae0188beff491efe1b104
Yael tal <yaelital27@gmail.com>
2023-01-21 15:03:49 +0200

    Frontend - adding loader

--- frontend/src/cmps/helpers/loader.jsx
+++ frontend/src/cmps/helpers/loader.jsx
@@ -1,17 +1,30 @@
 // import { useRef} from 'react'
 
+// export function Loader() {
+//   const loader = 'Loader1.svg'
+//   // const mountTime = useRef(Date.now());
+//   // const mountDelay = -(mountTime.current % 1000);
+
+//   return (
+//     // <h1>Loading...</h1>
+//     <img
+//       className="loader"
+//       src={loader2}
+//       alt="Loader"
+//       // style={{ '--loader-delay': `${mountDelay}ms` }}
+//     />
+//   )
+// }
+
+import loader2 from '../../assets/animation/loader/loader2.svg'
+
 export function Loader() {
-  const loader = 'Loader1.svg'
-  // const mountTime = useRef(Date.now());
-  // const mountDelay = -(mountTime.current % 1000);
 
   return (
-    // <h1>Loading...</h1>
     <img
       className="loader"
       src={loader2}
       alt="Loader"
-      // style={{ '--loader-delay': `${mountDelay}ms` }}
     />
   )
 }
