a276d5261968ce74e646d07c41c5517181e223ec
Yael tal <yaelital27@gmail.com>
2023-01-26 01:07:56 +0200

    Frontend - working on board service

--- frontend/src/services/board/board.service.local.js
+++ frontend/src/services/board/board.service.local.js
@@ -964,7 +964,6 @@ function _createBoard(title, backgroundImg, bgColor, isStarred) {
             },
             attachments: [],
             comments: [],
-            comments: [],
             checklists: [],
             style: {
               bgColor:
@@ -1470,7 +1469,6 @@ function getColors() {
 }
 
 function getCoverColors() {
-  return ['#7BC86C', '#F5DD29', '#FFAF3F', '#EF7564', '#CD8DE5', '#5BA4CF', '#29CCE5', '#6DECA9', '#FF8ED4', '#172B4D']
   return ['#7BC86C', '#F5DD29', '#FFAF3F', '#EF7564', '#CD8DE5', '#5BA4CF', '#29CCE5', '#6DECA9', '#FF8ED4', '#172B4D']
 }
 
