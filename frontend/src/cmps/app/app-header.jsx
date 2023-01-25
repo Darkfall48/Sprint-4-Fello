f731aecfa46b20b43a0333e980f562b49085771e
Darkfall48 <sidney@sebban.be>
2023-01-25 14:10:17 +0200

    Frontend - App - Fixed: App Name

--- frontend/src/cmps/app/app-header.jsx
+++ frontend/src/cmps/app/app-header.jsx
@@ -1,20 +1,18 @@
 //? Libraries
 import { Link, NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { CgMenuGridR } from 'react-icons/cg';
-import { FiChevronDown } from 'react-icons/fi';
-
+import { CgMenuGridR } from 'react-icons/cg'
+import { FiChevronDown } from 'react-icons/fi'
 
 //? Routes
 import routes from '../../routes'
-import { useState } from 'react';
-import { BoardAdd } from '../workspace/board-add';
-import { FastAverageColor } from 'fast-average-color';
-import { useRef } from 'react';
+import { useState } from 'react'
+import { BoardAdd } from '../workspace/board-add'
+import { FastAverageColor } from 'fast-average-color'
+import { useRef } from 'react'
 
 export function AppHeader() {
-
-  const fac = new FastAverageColor();
+  const fac = new FastAverageColor()
   const boards = useSelector((storeState) => storeState.boardModule.boards)
   const board = useSelector((storeState) => storeState.boardModule.board)
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -24,27 +22,42 @@ export function AppHeader() {
   }
 
   return (
-    <header className="app-header-section full" style={board?.style?.bgColor && { background: board.style.bgColor }}>
-      <Link to="/" ><CgMenuGridR className="header-links" /></Link>
-      <Link to="/board"  >
+    <header
+      className="app-header-section full"
+      style={board?.style?.bgColor && { background: board.style.bgColor }}
+    >
+      <Link to="/">
+        <CgMenuGridR className="header-links" />
+      </Link>
+      <Link to="/board">
         {/* <span className='logo'></span> */}
-        <img src="../assets/img/header/logo.png" alt="" />
-        <h2 className='logo-name' style={{ fontFamily: 'charlie-regular', color: 'white' }}>Frello</h2>
+        {/* <img src="../assets/img/header/logo.png" alt="" /> */}
+        <h2
+          className="logo-name"
+          style={{ fontFamily: 'charlie-regular', color: 'white' }}
+        >
+          Fello
+        </h2>
       </Link>
       <nav>
         {routes.map((route) => (
           <NavLink key={route.path} to={route.path}>
             {route.label}
-            <FiChevronDown className='down-arrow-nav' />
+            <FiChevronDown className="down-arrow-nav" />
           </NavLink>
         ))}
       </nav>
 
-      <div className='create-dropdown'>
-        <button onClick={() => { setIsModalOpen(!isModalOpen) }}>Create</button>
+      <div className="create-dropdown">
+        <button
+          onClick={() => {
+            setIsModalOpen(!isModalOpen)
+          }}
+        >
+          Create
+        </button>
         {isModalOpen && <BoardAdd onCloseModal={onCloseModal} />}
       </div>
-
     </header>
   )
 }
