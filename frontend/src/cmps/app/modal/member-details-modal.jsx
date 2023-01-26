1cb4d3e7dc843a62ed01c245416bc3b6e2552bcc
Darkfall48 <sidney@sebban.be>
2023-01-26 10:33:11 +0200

    Frontend - Task Preview - Set Members - Fixed Issues & Setting the stage for the modal

--- frontend/src/cmps/app/modal/member-details-modal.jsx
+++ frontend/src/cmps/app/modal/member-details-modal.jsx
@@ -1,12 +1,20 @@
-import { CgClose } from "react-icons/cg";
+import { CgClose } from 'react-icons/cg'
 
-export function MemberDetailsModal({ onCloseModal, board, task }) {
-    return <div className='modal-content' onClick={(e) => e.stopPropagation()} onBlur={() => onCloseModal}>
-
-        <div id="modal-header">
-            <h6>Keren Siebner</h6>
-            <button id='close-btn' onClick={onCloseModal}><CgClose /></button>
-        </div>
-        Member
+export function MemberDetailsModal({ onCloseModal, board, member }) {
+  console.log('Memberrrr', member)
+  return (
+    <div
+      className="modal-content"
+      onClick={(e) => e.stopPropagation()}
+      onBlur={() => onCloseModal}
+    >
+      <div id="modal-header">
+        <h6>{member.fullname}</h6>
+        <button id="close-btn" onClick={onCloseModal}>
+          <CgClose />
+        </button>
+      </div>
+      Member
     </div>
+  )
 }
