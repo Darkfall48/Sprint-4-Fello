d876d32586a4a2b3f0ab9c45051daf15e2dfcb3c
Darkfall48 <sidney@sebban.be>
2023-01-26 11:21:51 +0200

    Frontend - Task Preview - Set Cover - Fixed Code and now Pixel Perfect!

---
+++ frontend/src/cmps/board/group/task/cmps/set-cover.jsx
@@ -0,0 +1,20 @@
+export function SetCover({ task }) {
+  const { style } = task
+
+  if (style?.bgImg)
+    return (
+      <img
+        className="task-preview-cover-img"
+        src={style.bgImg}
+        alt="Card Image"
+      />
+    )
+  else if (style?.bgColor)
+    return (
+      <article
+        className="task-preview-cover-color"
+        style={{ background: style.bgColor }}
+      ></article>
+    )
+  else return <div className="task-preview-no-cover"></div>
+}
